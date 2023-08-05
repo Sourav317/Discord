c771c2e86d4c5fbaff0edbade6cf00930d09ba46
Sourav317 <kediasourav2@gmail.com>
2023-08-05 20:23:47 +0530

    Message is visible on chat and emits event when user join, leaves the room

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
 // import {express} from 'express';
 const path = require("path");
-const express = require('express');
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 const botName = "ChatCord Bot";
 
+// Run when client connects
+io.on("connection", (socket) => {
+    console.log('New connection');
+  // Welcome current user
+  socket.emit("message", "Welcome to ChatCord!");
+
+  // Broadcast when a user connects
+  socket.broadcast.emit("message", "A user has joined the chat");
+
+  // Runs when client disconnects
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left the chat");
+  });
+
+  // Listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    io.emit("message", msg);
+  });
+});
 
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT,() => {console.log(`server is running on port ${PORT}`)});
+server.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
